dc8eea9cf19a2b98a2152decdc9c575bfd58ca96
amirrezaakbarimehr <152810341+amirrezaakbarimehr@users.noreply.github.com>
2023-12-05 22:32:25 +0330

    gitign

--- calculator.c
+++ calculator.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int calculate( int a, int b) {
+<<<<<<< HEAD
 // A combination of both calculations
 return a * b * 2 -10 + (a + b);
 }
+=======
+// Some calculation
+return a * b;
+}
+>>>>>>> a3a8574 ( calculator.c)
